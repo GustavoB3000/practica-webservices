aa58f3158bdbebca051528a995912ebabfa97b3f
Gustavo Bearzi <gustavobearzi2012@gmail.com>
2023-02-26 11:39:18 -0300

    PRACTICA 7: Finalizada. Se realizo correctamente el uso de los metodos GET y POST.

--- src/main/java/com/devpredator/practicawebservices/service/RestauranteService.java
+++ src/main/java/com/devpredator/practicawebservices/service/RestauranteService.java
@@ -3,10 +3,75 @@
  */
 package com.devpredator.practicawebservices.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devpredator.practicawebservices.entity.Restaurante;
+
 /**
  * @author 4PF28LA_2004
  *
  */
 public class RestauranteService {
 
+	public List<Restaurante> consultarRestaurantes() {
+		// Restaurante 1:
+		Restaurante r1 = new Restaurante();
+		r1.setIdRestaurante(1L);
+		r1.setNombre("La Perla 1.");
+		r1.setDireccion("Las serranas 400.");
+		r1.setSlogan("Slogan 1.");
+
+		// Restaurante 2:
+		Restaurante r2 = new Restaurante();
+		r2.setIdRestaurante(2L);
+		r2.setNombre("La Perla 2.");
+		r2.setDireccion("Las serranas 402.");
+		r2.setSlogan("Slogan 2.");
+
+		// Restaurante 3:
+		Restaurante r3 = new Restaurante();
+		r3.setIdRestaurante(3L);
+		r3.setNombre("La Perla 3.");
+		r3.setDireccion("Las serranas 403.");
+		r3.setSlogan("Slogan 3.");
+
+		// Restaurante 4:
+		Restaurante r4 = new Restaurante();
+		r4.setIdRestaurante(4L);
+		r4.setNombre("La Perla 4.");
+		r4.setDireccion("Las serranas 404.");
+		r4.setSlogan("Slogan 4.");
+
+		// Restaurante 5:
+		Restaurante r5 = new Restaurante();
+		r5.setIdRestaurante(5L);
+		r5.setNombre("La Perla 5.");
+		r5.setDireccion("Las serranas 405.");
+		r5.setSlogan("Slogan 5.");
+		
+		//Listado de restaurante:
+		List<Restaurante> restaurantes = new ArrayList<>();
+		
+		restaurantes.add(r1);
+		restaurantes.add(r2);
+		restaurantes.add(r3);
+		restaurantes.add(r4);
+		restaurantes.add(r5);
+
+		return restaurantes;
+	}
+	
+	public Restaurante consultarRestaurante(Long id) {
+		List<Restaurante> restaurantes = this.consultarRestaurantes();
+		
+		Restaurante rs = null;
+		
+		for (Restaurante restaurante : restaurantes) {
+			if(id == restaurante.getIdRestaurante()) {
+				rs = restaurante;
+			}
+		}
+		return rs;
+	}
 }
--- src/main/java/com/devpredator/practicawebservices/ws/RestaurantesWS.java
+++ src/main/java/com/devpredator/practicawebservices/ws/RestaurantesWS.java
@@ -3,10 +3,75 @@
  */
 package com.devpredator.practicawebservices.ws;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.devpredator.practicawebservices.entity.Restaurante;
+import com.devpredator.practicawebservices.service.RestauranteService;
+
 /**
  * @author 4PF28LA_2004
  *
  */
+@Path("restaurantesWS")
 public class RestaurantesWS {
 
+	private RestauranteService rs = new RestauranteService();
+	
+	@GET
+	@Path("consultarRestaurantes")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Restaurante> consultarRestaurantes() {
+		return this.rs.consultarRestaurantes();
+				
+	}
+	
+	@GET
+	@Path("consultarRestaurante/{numEmp}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response consultarEmpleadoPorId(@PathParam("numEmp") Long numero) {
+		Restaurante em =  this.rs.consultarRestaurante(numero);
+		
+		if(em == null) {
+			return Response.noContent().build();
+		}
+		
+		GenericEntity<Restaurante> empleadoGenerico = new GenericEntity<Restaurante>(em, Restaurante.class);
+		
+		return Response.ok(empleadoGenerico.getEntity()).build();
+	}
+	
+	@POST
+	@Path("guardarRestaurante")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response guardarRestaurante(Restaurante restaurante) {
+		
+		if(restaurante == null) {
+			return Response.status(400).entity("No se ha ingresado dato alguno del Restaurante. Favor de ingresar datos").build();
+		}
+		
+		if(restaurante.getIdRestaurante() == null ) {
+			return Response.status(400).entity("El ID del Restaurante es obligatorio, favor de ingresarlo.").build();
+		}
+		
+		
+		if(restaurante.getNombre() == null || restaurante.getNombre().isEmpty()) {
+			return Response.status(400).entity("El nombre del Restaurante es obligatorio, favor de ingresar uno.").build();
+		}
+		
+		GenericEntity<Restaurante> restauranteGenerico = new GenericEntity<Restaurante>(restaurante, Restaurante.class);
+		return Response.ok(restauranteGenerico).build();
+	}
 }
